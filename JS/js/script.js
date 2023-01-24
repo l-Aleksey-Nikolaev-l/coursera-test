364c6603854b9ffd3881f483518c36bc17bb234e
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2023-01-24 15:36:51 +0100

    ver 0.0.2 - Primitives

--- JS/js/script.js
+++ JS/js/script.js
@@ -1,19 +1,23 @@
-let message = "in global";
-console.log("global: message = " + message);
+let x;
+console.log(x);
 
-let a = function () {
-    let message = "inside a";
-    console.log("a: message = " + message);
-
-    function b() {
-        console.log("b: message = " + message);
-    }
-    b();
-    c();
+if (x === undefined) {
+    console.log("x is undefined");
 }
 
-function c() {
-    console.log("c: message = " + message);
+x = 5;
+
+if (x === undefined) {
+    console.log("x is undefined");
+} else {
+    console.log("x has been defined");
 }
 
-a();
+// 6 general primitives
+
+// boolean: true and false
+// symbol: "F", "C", etc
+// number: 1, 123, 2134353
+// string: "Hello world"
+// null
+// undefined
