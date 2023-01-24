f33eb592190fee3fb81dbee8554f428285d3ae3a
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2023-01-24 15:23:52 +0100

    ver 0.0.1

--- JS/js/script.js
+++ JS/js/script.js
@@ -1,1 +1,19 @@
-let x = "Hello world!";
+let message = "in global";
+console.log("global: message = " + message);
+
+let a = function () {
+    let message = "inside a";
+    console.log("a: message = " + message);
+
+    function b() {
+        console.log("b: message = " + message);
+    }
+    b();
+    c();
+}
+
+function c() {
+    console.log("c: message = " + message);
+}
+
+a();
