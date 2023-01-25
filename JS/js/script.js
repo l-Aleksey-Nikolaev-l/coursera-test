b391356af01d8214879c2b8b262d9029f8f19ad3
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2023-01-25 18:15:17 +0100

    ver 0.0.6 - Object literal

--- JS/js/script.js
+++ JS/js/script.js
@@ -1,10 +1,25 @@
 // Object creation
-let company = new Object();
+// let company = new Object();
+//
+// company.name = "Facebook";
+// company.ceo = new Object();
+// company.ceo.firstName = "Mark";
+// company.ceo.favColor = "Blue";
+//
+// console.log(company["name"]);
+// console.log("Company CEO name is: " + company.ceo.firstName);
 
-company.name = "Facebook";
-company.ceo = new Object();
-company.ceo.firstName = "Mark";
-company.ceo.favColor = "Blue";
 
-console.log(company["name"]);
-console.log("Company CEO name is: " + company.ceo.firstName);
+
+// Better way: object literal
+
+let facebook = {
+    name: "Facebook",
+    ceo: {
+        firstName: "Mark",
+        favColor: "Blue"
+    },
+    "stock of company": 110
+};
+
+console.log(facebook);
