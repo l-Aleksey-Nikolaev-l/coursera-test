85088141654b863c24258e0cf72742f7eb1adc69
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2023-01-26 11:13:23 +0100

    ver 0.0.13 - Closures

--- JS/js/script.js
+++ JS/js/script.js
@@ -1,64 +1,16 @@
-// Arrays
+// Closures
 
-// let myArray = new Array();
-// myArray[0] = "Aleksey";
-// myArray[1] = 2;
-// myArray[2] = function (name) {
-//     console.log("Hello " + name);
-// };
-// myArray[3] = {course: "HTML, CSS & JavaScript"};
-//
-//
-// myArray[2](myArray[0]);
-//
-// console.log(myArray[3].course);
-
-
-
-
-
-// Shorter version
-
-// let myArray2 = [
-//     "Aleksey",
-//     "John",
-//     "Mark"
-// ];
-//
-// myArray2[100] = "Bob";
-//
-// for (let i = 0; i < myArray2.length; i++) {
-//     console.log(i + " - Hello " + myArray2[i]);
-// }
-
-
-
-
-
-
-// let myObj = {
-//     name: "Aleksey",
-//     course: "HTML/CSS/JS",
-//     platform: "Coursera"
-// };
-//
-// for (let prop in myObj) {
-//     console.log(prop + " - " + myObj[prop]);
-// }
-
-
-
-
-
-
-
-
-let myArray3 = ["Aleksey", "John", "Mark"];
-myArray3.greeting = "Hi!";
-
-
-for (let name in myArray3) {
-    console.log("Hello " + myArray3[name]);
+function makeMultiplier (multiplier) {
+    return (
+        function (x) {
+            return multiplier * x;
+        }
+    );
 }
 
+
+
+let doubleAll = makeMultiplier(2);
+
+console.log(doubleAll(10));
 
