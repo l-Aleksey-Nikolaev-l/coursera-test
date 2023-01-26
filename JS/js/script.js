c97d63304a13193c7843e6e52b38eaedb0210803
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2023-01-26 10:48:05 +0100

    ver 0.0.11 - Arrays

--- JS/js/script.js
+++ JS/js/script.js
@@ -1,25 +1,31 @@
-// Object Literals and the 'this' Keyword
+// Arrays
 
-let literalCircle = { // new Object()
-    radius: 10,
+// let myArray = new Array();
+// myArray[0] = "Aleksey";
+// myArray[1] = 2;
+// myArray[2] = function (name) {
+//     console.log("Hello " + name);
+// };
+// myArray[3] = {course: "HTML, CSS & JavaScript"};
+//
+//
+// myArray[2](myArray[0]);
+//
+// console.log(myArray[3].course);
 
-    getArea: function () {
 
-        let self = this;
+// Shorter version
+let myArray2 = [
+    "Aleksey",
+    "John",
+    "Mark"
+];
 
-        console.log(this); // The 'this' word works only inside this object
+myArray2[100] = "Bob";
 
-        let increaseRadius = function () {
-            self.radius = 20;
-        };
+for (let i = 0; i < myArray2.length; i++) {
+    console.log(i + " - Hello " + myArray2[i]);
+}
 
-        increaseRadius();
-        console.log(this.radius);
-
-        return Math.PI * Math.pow(this.radius, 2);
-    }
-};
-
-console.log(literalCircle.getArea());
 
 
