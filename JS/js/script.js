50cc5c875860ce6ead8736cd5d2b4e636918bd4e
Aleksey Nikolaev <senatorfirst1@gmail.com>
2023-01-30 10:41:30 +0100

    ver 0.0.19 - DOM Manipulation

--- JS/js/script.js
+++ JS/js/script.js
@@ -1,7 +1,37 @@
 
 
+function sayHello (){
+    let userName = document.getElementById("name").value;
 
-console.log(document.getElementById("title"));
+    const nameValidator = new RegExp("[A-Za-z]");
+    const nameTest = nameValidator.test(userName);
+
+    if (nameTest === true) {
+
+        let message = "<h2>Hello " + userName + "!</h2>";
+
+        document
+            .getElementById("content")
+            .innerHTML = message;
+
+        if (userName === "student") {
+            let title =
+                document
+                    .querySelector("#title")
+                    .textContent;
+            title += " & Loving' it!"
+            document
+                .querySelector("#title")
+                .textContent = title;
+        }
+
+    } else {
+        document
+            .getElementById("content")
+            .textContent = "";
+    }
+
+}
 
 
 
