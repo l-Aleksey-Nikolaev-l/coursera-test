f33685e64e9ebc8333c7be30b0915a152bba0103
Aleksey Nikolaev <senatorfirst1@gmail.com>
2023-01-30 09:51:33 +0100

    ver 0.0.18 - DOM Manipulation

--- JS/js/script.js
+++ JS/js/script.js
@@ -1,14 +1,7 @@
-(function (window) {
-    let AlexGreeter = {};
-    AlexGreeter.name = "Alex";
-    let greeting = "Hello ";
-    AlexGreeter.sayHello = function () {
-        console.log(greeting + AlexGreeter.name);
-    }
 
-    window.AlexGreeter = AlexGreeter;
 
-})(window);
+
+console.log(document.getElementById("title"));
 
 
 
