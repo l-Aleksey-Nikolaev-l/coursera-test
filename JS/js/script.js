28ca2fd16feafaa149b1af198525b3a2a471ac84
Aleksey Nikolaev <senatorfirst1@gmail.com>
2023-01-31 12:21:58 +0100

    ver 0.0.23 - Ajax

--- JS/js/script.js
+++ JS/js/script.js
@@ -1,69 +1,16 @@
-
-
-
 document.addEventListener("DOMContentLoaded",
     function (event) {
-        function sayHello (event) {
-            console.log(event);
 
-            let userName = document.getElementById("name").value;
+        document.querySelector("button")
+            .addEventListener("click", function () {
 
-            const nameValidator = new RegExp("[A-Za-z]");
-            const nameTest = nameValidator.test(userName);
+                $ajaxUtils.sendGetRequest("/data/name.txt",
+                        function (request) {
+                        let name = request.responseText;
 
-            if (nameTest === true) {
-
-                this.textContent = "Said It!"
-
-                let message = "<h2>Hello " + userName + "!</h2>";
-
-                document
-                    .getElementById("content")
-                    .innerHTML = message;
-
-                if (userName === "student") {
-                    let title =
-                        document
-                            .querySelector("#title")
-                            .textContent;
-                    title += " & Loving' it!"
-                    document
-                        .querySelector("#title")
-                        .textContent = title;
-                }
-
-            } else {
-                this.textContent = "Say it!"
-                document
-                    .getElementById("content")
-                    .textContent = "";
-            }
-
-        }
-
-
-
-// Unobtrusive event binding
-
-        document
-            .querySelector("button")
-            .addEventListener("click", sayHello);
-// or
-
-// document.querySelector("button")
-//     .onclick = sayHello;
-
-
-        document
-            .querySelector("body")
-            .addEventListener("mousemove",
-                function (event) {
-                if (event.shiftKey === true) {
-                    console.log("X: " + event.clientX);
-                    console.log("Y: " + event.clientY);
-                }
+                        document.querySelector("#content")
+                            .innerHTML = "<h2>Hello " + name + "!";
+                        });
             });
     }
 );
-
-
